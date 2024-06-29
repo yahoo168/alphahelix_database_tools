6d89b69755a9156383656a7b7ae477a82185b1e6
yahoo168 <a86305394@gmail.com>
2024-06-29 16:19:44 +0800

    integrate google tools object

--- external_tools/google_tools.py
+++ external_tools/google_tools.py
@@ -1,37 +1,55 @@
-import os, re
-from datetime import datetime
+import os
 from google.cloud import storage
 import concurrent.futures
 
 class GoogleCloudStorageTools():
-    def __init__(self, credential_file_path=None):
-        if credential_file_path:
-            os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = credential_file_path
-        
-    # 待調用函數: 用於上傳file or file_name物件類型的檔案
+    def __init__(self, credential_file_path):
+        # 設定google cloud storage的認證檔案路徑
+        os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = credential_file_path
+        self.self.storage_client = storage.Client()
+    
+    # 給定bucket名稱和blob名稱，返回blib物件
+    def get_blob(self, bucket_name, blob_name):
+        """Get a blob from the bucket."""
+        bucket = self.storage_client.bucket(bucket_name)
+        return bucket.get_blob(blob_name)
+    
+    # 指定folder，取得其中的blob列表
+    def get_blob_list_in_folder(self, bucket_name, folder_name):
+        """Lists all the files in the specified folder in the bucket."""
+        bucket = self.storage_client.bucket(bucket_name)
+        # 加上斜杠以確保只查詢資料夾內的文件
+        # 若folder_name 本來沒有/，以下函數可確保它有斜杠，若原本已有，rstrip('/') 會先移除然後再添加一個斜杠。
+        prefix = folder_name.rstrip('/') + '/'
+        blob_list = list()
+        # 透過prefix參數，只列出指定資料夾下的blob
+        for blob in bucket.list_blobs(prefix=prefix):
+            # 若blob不是資料夾，則加入blob_list（GCS的資料夾是以/結尾的blob）
+            if not blob.name.endswith('/'):
+                blob_list.append(blob)
+        return blob_list
+     
+    # 待調用函數: 用於上傳file/file_name物件類型的檔案，前者是實際的檔案物件，後者是檔案的路徑
     def _upload_to_bucket(self, bucket_name, blob_data):
         """Uploads a file to the bucket and returns the public URL."""
-        blob_name = blob_data["blob_name"]
-        file_type = blob_data["file_type"]  # "file" or "file_name"
-        src_file = blob_data["file"]
-        storage_client = storage.Client()
-        bucket = storage_client.bucket(bucket_name)
-        blob = bucket.blob(blob_name)
-
-        # Set custom metadata if present
+        blob_name, file_type, src_file = blob_data["blob_name"], blob_data["file_type"], blob_data["file"]
+        blob = self.get_blob(bucket_name, blob_name)
+        # 若存在metadata，則將metadata加入blob（GCS稱為中繼資料）
         if "metadata" in blob_data:
             blob.metadata = blob_data["metadata"]
-
-        # Upload the file based on the file_type
+        # 依照檔案類型選擇不同上傳方式
         if file_type == "file":
             blob.upload_from_file(src_file)
         elif file_type == "file_name":
             blob.upload_from_filename(src_file)
-
+        # 設定blob為公開，並返回blob名稱和公開的url
         blob.make_public()
         return blob_name, blob.public_url
         
-    # 將檔案上傳到google_cloud_storage，file可為file_name或file物件
+    # 使用multi-thread，將檔案上傳到google_cloud_storage，檔案類型可為file_name / file物件
+    # 須傳入bucket名稱和blob_meta_list，blob_meta_list是一個list
+    # 每個元素是一個dict，包含blob_name, file_type, file, metadata等key（metadata為可選，是1個dict）
+    # file_type若為file_name，則file是檔案路徑；若為file，則file是檔案物件
     def upload_to_google_cloud_storage(self, bucket_name, blob_meta_list):
         blob_url_dict = {}
         with concurrent.futures.ThreadPoolExecutor() as executor:
@@ -49,55 +67,29 @@ class GoogleCloudStorageTools():
                     print(f"Error uploading file {blob_name}: {e}")
 
         return blob_url_dict
-
-    def get_blob(self, bucket_name, blob_name):
-        """Get a blob from the bucket."""
-        storage_client = storage.Client()
-        bucket = storage_client.bucket(bucket_name)
-        return bucket.get_blob(blob_name)
-
-    def get_blob_list_in_folder(self, bucket_name, folder_name):
-        """Lists all the files in the specified folder in the bucket."""
-        storage_client = storage.Client()
-        bucket = storage_client.bucket(bucket_name)
-        
-        # 加上斜杠以確保只查詢資料夾內的文件
-        # 如果 folder_name 本來就沒有斜杠，這樣做可以確保它有一個斜杠，如果 folder_name 已經有斜杠，rstrip('/') 會先移除它，然後再添加一個斜杠。
-        prefix = folder_name.rstrip('/') + '/'
-        blob_list = list()
-        for blob in bucket.list_blobs(prefix=prefix):
-            if not blob.name.endswith('/'):
-                blob_list.append(blob)
-        return blob_list
-
+    
+    # 設置blob的metadata
     def set_blob_metadata(self, bucket_name, blob_name, metadata):
         """Set a blob's metadata."""
-        storage_client = storage.Client()
-        bucket = storage_client.bucket(bucket_name)
-        blob = bucket.get_blob(blob_name)
+        blob = self.get_blob(bucket_name, blob_name)
+        # 會針對已經有的metadata進行更新，而非直接覆蓋
         blob.metadata = metadata
         blob.patch()
 
-    # 从 Google Cloud Storage 下载文件
+    # 從Google Cloud Storage下载文件到本地
     def download_blob(self, bucket_name, blob_name, file_path):
-        storage_client = storage.Client()
-        bucket = storage_client.bucket(bucket_name)
-        blob = bucket.blob(blob_name)
+        blob = self.get_blob(bucket_name, blob_name)
         blob.download_to_filename(file_path)
         print(f"Blob {blob_name} is downloaded to {file_path}.")
-
+        
+    # 將blob設為公開
     def make_blob_public(self, bucket_name, blob_name):
-        """将 GCS 上的 blob 设置为公开访问"""
-        client = storage.Client()
-        bucket = client.bucket(bucket_name)
-        blob = bucket.blob(blob_name)
+        blob = self.get_blob(bucket_name, blob_name)
         blob.make_public()
         return blob.public_url
-
+    
+    # 生成一個預設過期時間1小時（3600秒）的簽名URL，可確保用戶以任何方式訪問（目前用於取得memo文字）
     def generate_signed_url(self, bucket_name, blob_name, expiration=3600):
-        """生成一个带有过期时间的签名 URL"""
-        client = storage.Client()
-        bucket = client.bucket(bucket_name)
-        blob = bucket.blob(blob_name)
+        blob = self.get_blob(bucket_name, blob_name)
         url = blob.generate_signed_url(expiration=expiration)
         return url
