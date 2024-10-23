b034b7d689c55185b3038cf92a32755ded395c8f
yahoo168 <a86305394@gmail.com>
2024-10-23 21:50:53 +0800

    add image processing module

--- alphahelix_database_tools/external_tools/openai_tools.py
+++ alphahelix_database_tools/external_tools/openai_tools.py
@@ -1,5 +1,7 @@
 from openai import OpenAI # type: ignore
 import tiktoken
+import base64
+import json
 
 # 待改：multi-thread
 def call_OpenAI_API(API_key, prompt, model_version="gpt-4o", output_format="text"):
@@ -15,7 +17,38 @@ def call_OpenAI_API(API_key, prompt, model_version="gpt-4o", output_format="text
         response_format={"type": output_format}
     )
     return chat_completion.choices[0].message.content
+    
+def call_OpenAI_for_image(API_key, text_prompt, image_path_list, is_local_file=False, model="gpt-4o-mini", max_tokens=None, output_format="json_object"):
+    def encode_image(image_path):
+        """Encode a local image file to base64."""
+        with open(image_path, "rb") as image_file:
+            return base64.b64encode(image_file.read()).decode('utf-8')
+    
+    client = OpenAI(api_key=API_key)
+    messages = [
+        {
+            "role": "user",
+            "content": [
+                {"type": "text", "text": text_prompt},
+            ] + [
+                {
+                    "type": "image_url",
+                    "image_url": {
+                        "url": f"data:image/jpeg;base64,{encode_image(image_path)}" if is_local_file else image_path
+                    },
+                }
+                for image_path in image_path_list
+            ]
+        }
+    ]
 
+    response = client.chat.completions.create(
+        model=model,
+        messages=messages,
+        max_tokens=max_tokens,
+        response_format={"type": output_format},
+    )
+    return response.choices[0].message.content
 
 def cal_GPT_API_expense(text, model="gpt-4o"):
     # 計算token數，"cl100k_base"是與GPT-3和GPT-4模型兼容的分詞器。
